7f614723750e584fb1da42750c96f2505bcc79ec
naresh277 <nareshkch27@gmail.com>
2022-10-14 21:58:26 +0545

    function reused

--- src/components/Newsbox.js
+++ src/components/Newsbox.js
@@ -1,21 +1,16 @@
-import React, { Component } from 'react'
-import { Newsdescription } from './Newsdescription'
-
-
+import React, { Component } from "react";
+import { Newsdescription } from "./Newsdescription";
 
 export class Newsbox extends Component {
-
-
   constructor() {
     super();
     this.state = {
       articles: [],
-      page: 1
-    }
+      page: 1,
+    };
   }
 
-
-  async componentDidMount() {
+  async updateNews() {
     try {
       this.props.setprogress(10);
       const url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=c360473e1dc0405e9faba04b3ecfa1e6&page=1&pageSize=6`;
@@ -23,73 +18,68 @@ export class Newsbox extends Component {
       this.props.setprogress(50);
       const data = await res.json();
       this.setState({
-        articles: data.articles
+        articles: data.articles,
       });
       this.props.setprogress(100);
-    }
-    catch (e) {
+    } catch (e) {
       console.log("something is not working");
     }
+  }
+
+  async componentDidMount() {
+    this.updateNews();
   }
 
   handleOnClickPrev = async () => {
-    try {
-      const url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=c360473e1dc0405e9faba04b3ecfa1e6&page=${this.state.page - 1}&pageSize=6`;
-      const res = await fetch(url);
-      const data = await res.json();
-      this.setState({
-        articles: data.articles,
-        page: this.state.page - 1
-      });
-    }
-    catch (e) {
-      console.log("something is not working");
-    }
-  }
+    this.updateNews();
+  };
 
   handleOnClickNext = async () => {
-    try {
-      const url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=c360473e1dc0405e9faba04b3ecfa1e6&page=${this.state.page + 1}&pageSize=6`;
-      const res = await fetch(url);
-      const data = await res.json();
-      this.setState({
-        articles: data.articles,
-        page: this.state.page + 1
-      });
-    }
-    catch (e) {
-      console.log("something is not working");
-    }
-  }
-  render() {
+    this.updateNews();
+  };
 
+  render() {
     return (
       <>
-
-        <div className='container my-3'>
-          <div className='row'>
+        <div className="container my-3">
+          <div className="row">
             {this.state.articles.map((element) => {
               return (
                 <>
-                  <div className='col-md-4' key={"element.url"}>
-                    <Newsdescription title={element.title} description={element.description} imgUrl={element.urlToImage} link={element.url} />
+                  <div className="col-md-4" key={"element.url"}>
+                    <Newsdescription
+                      title={element.title}
+                      description={element.description}
+                      imgUrl={element.urlToImage}
+                      link={element.url}
+                    />
                   </div>
                 </>
-              )
-
+              );
             })}
-            <div className="d-flex justify-content-between my-3" >
-              <button type="button" disabled={this.state.page <= 1} onClick={this.handleOnClickPrev} className="btn btn-dark">Previous</button>
-              <button type="button" onClick={this.handleOnClickNext} className="btn btn-dark">Next</button>
+            <div className="d-flex justify-content-between my-3">
+              <button
+                type="button"
+                disabled={this.state.page <= 1}
+                onClick={this.handleOnClickPrev}
+                className="btn btn-dark"
+              >
+                Previous
+              </button>
+              <button
+                type="button"
+                onClick={this.handleOnClickNext}
+                className="btn btn-dark"
+              >
+                Next
+              </button>
             </div>
           </div>
         </div>
-        <div>
-
-        </div>
+        <div></div>
       </>
-    )
+    );
   }
 }
 
-export default Newsbox
+export default Newsbox;
