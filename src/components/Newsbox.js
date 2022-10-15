984f6522856677b0e1414eaae0249aaf96614403
naresh277 <nareshkch27@gmail.com>
2022-10-15 14:47:41 +0545

    infinite scroll added

--- src/components/Newsbox.js
+++ src/components/Newsbox.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import { Newsdescription } from "./Newsdescription";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export class Newsbox extends Component {
   constructor() {
     super();
     this.state = {
       articles: [],
-      page: 1,
+      page: 2,
+      totalResults : 0,
+      loading : true
     };
   }
 
@@ -19,29 +22,58 @@ export class Newsbox extends Component {
       const data = await res.json();
       this.setState({
         articles: data.articles,
+        totalResults : data.totalResults,
+        loading : true
       });
       this.props.setprogress(100);
     } catch (e) {
       console.log("something is not working");
     }
   }
+   
 
   async componentDidMount() {
     this.updateNews();
   }
+  fetchMoreData =async ()=>{
+    this.setState({
+      page : this.state.page+1,
+    })
+      this.props.setprogress(10);
+      const url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=c360473e1dc0405e9faba04b3ecfa1e6&page=${this.state.page}&pageSize=6`;
+      const res = await fetch(url);
+      this.props.setprogress(50);
+      const data = await res.json();
+      this.setState({
+        
+        articles: this.state.articles.concat(data.articles),
+        totalResults : data.totalResults, 
+        loading : true,
+        
+      })
+      this.props.setprogress(100);
+  }
+  // handleOnClickPrev = async () => {
+  //   this.updateNews();
+  // };
 
-  handleOnClickPrev = async () => {
-    this.updateNews();
-  };
-
-  handleOnClickNext = async () => {
-    this.updateNews();
-  };
+  // handleOnClickNext = async () => {
+  //   this.updateNews();
+  // };
 
   render() {
     return (
       <>
+
         <div className="container my-3">
+       
+        <div className="container">
+        <InfiniteScroll
+              dataLength={this.state.articles.length}
+              next={this.fetchMoreData}
+              hasMore={this.state.articles.length !== this.state.totalResults}
+              loader={<h4>Loading...</h4>}
+            >
           <div className="row">
             {this.state.articles.map((element) => {
               return (
@@ -54,10 +86,11 @@ export class Newsbox extends Component {
                       link={element.url}
                     />
                   </div>
+                  
                 </>
               );
             })}
-            <div className="d-flex justify-content-between my-3">
+            {/* <div className="d-flex justify-content-between my-3">
               <button
                 type="button"
                 disabled={this.state.page <= 1}
@@ -73,10 +106,15 @@ export class Newsbox extends Component {
               >
                 Next
               </button>
-            </div>
+            </div> */}
+            
           </div>
+          </InfiniteScroll>
+          </div>
+          
         </div>
-        <div></div>
+        
+       
       </>
     );
   }
